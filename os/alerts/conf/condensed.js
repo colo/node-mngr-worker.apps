24f4eabff34013d8c7765162a05112a0a9941984
Luciano Bueno <glamero@gmail.com>
2018-06-13 18:52:50 -0300

    bump

--- os/alerts/conf/condensed.js
+++ os/alerts/conf/condensed.js
@@ -9,17 +9,23 @@ module.exports = {
     console.log('each host alert', value, payload)
   },
 
-  // 'data[].%hosts.os.loadavg': {
-  //   '$payload': {
-  //     'tabular[].%hosts.os.loadavg': (value, payload) => {
-  //       console.log('$playload loadavg alert', value)
-  //       return { 'value': value, 'property': payload.property }
-  //     }
-  //   },
-  //   '$callback': (value, payload) => {
-  //     console.log('loadavg alert', value, payload, payload.extra)
-  //   }
-  // },
+  'data[].%hosts.os.loadavg': {
+    '$payload': {
+      'buffer': [],
+      'counter': 0,
+      '$extra':{
+        'tabular[].%hosts.os.loadavg': (value, payload) => {
+          console.log('$playload loadavg alert', value)
+          return { 'value': value, 'property': payload.property }
+        }
+      }
+    },
+    '$callback': (value, payload) => {
+      payload.counter++
+      console.log('payload.extra[0]', payload.extra[0][0].value)
+      console.log('loadavg alert', value, payload)
+    }
+  },
 
 
   'data[].%hosts.%path.%properties[].%property': (value, payload) => {
--- os/alerts/conf/expanded.js
+++ os/alerts/conf/expanded.js
@@ -15,29 +15,35 @@ module.exports = {
     //   ]
     // },
 
-    {
-      '%hosts': {
-        'os' : {
-          'loadavg': {
-            '$payload': {
-              'tabular': [{
-                '%hosts': {
-                  'os' : {
-                    'loadavg': (value, payload) => {
-                      console.log('$playload loadavg alert', value)
-                      return { 'value': value, 'property': payload.property }
-                    }
-                  }
-                }
-              }]
-            },
-            '$callback': (value, payload) => {
-              console.log('loadavg alert', value, payload, payload.extra)
-            }
-          }
-        }
-      }
-    },
+    // {
+    //   '%hosts': {
+    //     'os' : {
+    //       'loadavg': {
+    //         '$payload': {
+    //           'buffer': [],
+    //           'counter': 0,
+    //           '$extra':{
+    //             'tabular': [{
+    //               '%hosts': {
+    //                 'os' : {
+    //                   'loadavg': (value, payload) => {
+    //                     console.log('$playload loadavg alert', value)
+    //                     return { 'value': value, 'property': payload.property }
+    //                   }
+    //                 }
+    //               }
+    //             }]
+    //           }
+    //         },
+    //         '$callback': (value, payload) => {
+    //           payload.counter++
+    //           console.log('payload.extra[0]', payload.extra[0][0].value)
+    //           console.log('loadavg alert', value, payload)
+    //         }
+    //       }
+    //     }
+    //   }
+    // },
 
     // {
     //   '%hosts': {
