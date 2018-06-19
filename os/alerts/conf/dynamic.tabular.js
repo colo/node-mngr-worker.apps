fbbb70c2fbff71e49fd0e6d587f33aba8fb2c21d
Luciano Bueno <glamero@gmail.com>
2018-06-18 23:20:51 -0300

    bump

--- os/alerts/conf/dynamic.tabular.js
+++ os/alerts/conf/dynamic.tabular.js
@@ -23,7 +23,7 @@ module.exports = {
         * @trasnform: diff between each value against its prev one
         */
         transform: function(values, caller, chart){
-          console.log('transform: ', values)
+          // console.log('transform: ', values)
 
           let transformed = []
           // let prev = {idle: 0, total: 0, timestamp: 0 },
@@ -75,7 +75,7 @@ module.exports = {
       /**
       * @var: save prev cpu data, need to calculate current cpu usage
       **/
-      prev: {idle: 0, total: 0, timestamp: 0 },
+      prev: {idle: 0, total: 0, timestamp: 0, value: 0 },
       /** **/
       watch: {
         /**
@@ -88,51 +88,64 @@ module.exports = {
         /**
         * returns  a bigger array (values.length * samples.length) and add each property
         */
-        transform: function(values){
-          console.log('cpus_minute_percentage transform: ', values)
+        transform: function(values, caller, chart){
+          // console.log('cpus_minute_percentage transform: ', values)
 
 
-          // let transformed = []
-          //
-          // Array.each(values, function(val, index){
-          //   // let transform = { timestamp: val.timestamp, value: (val.value / 1024) / 1024 }
-          //   // transformed.push(transform)
-          //
-          //   let last_sample = null
-          //   let counter = 0
-          //   Object.each(val.value.samples, function(sample, timestamp){
-          //     let transform = {timestamp: timestamp * 1, value: {samples: sample}}
-          //
-          //     if(counter == Object.getLength(val.value.samples) -1)
-          //       last_sample = sample
-          //
-          //     Object.each(val.value, function(data, property){
-          //       if(property != 'samples')
-          //         transform.value[property] = data
-          //     })
-          //
-          //     transformed.push(transform)
-          //     counter++
-          //   })
-          //
-          //   let timestamp = val.timestamp
-          //   let transform = {timestamp: timestamp * 1, value: {}}
-          //
-          //   Object.each(val.value, function(data, property){
-          //     if(property != 'samples'){
-          //       transform.value[property] = data
-          //     }
-          //     else{
-          //       transform.value['samples'] = last_sample
-          //     }
-          //   })
-          //   transformed.push(transform)
-          // })
-          //
-          // // //console.log('transformed: ', transformed)
-          // //
-          // return transformed
-          return values
+          let transformed = []
+          // let prev = {idle: 0, total: 0, timestamp: 0 },
+          Array.each(values, function(val, index){
+            let transform = {timestamp: val.timestamp, value: 0 }
+            let current = {idle: 0, total: 0, timestamp: val.timestamp }
+
+            if(current.timestamp > chart.prev.timestamp){
+              // if(index == 0){
+              Object.each(val.value, function(stat, key){
+                key = key.replace('times.', '').replace('.median', '')
+
+                if(key == 'idle')
+                  current.idle += stat
+
+                  current.total += stat
+              })
+
+
+              let diff_time = current.timestamp - chart.prev.timestamp
+              let diff_total = current.total - chart.prev.total;
+              let diff_idle = current.idle - chart.prev.idle;
+
+              // console.log('transform: ', current, chart.prev)
+
+              //algorithm -> https://github.com/pcolby/scripts/blob/master/cpu.sh
+              let percentage =  (diff_time * (diff_total - diff_idle) / diff_total ) / (diff_time * 0.01)
+
+              // if(percentage > 100){
+              //   //console.log('cpu transform: ', diff_time, diff_total, diff_idle)
+              // }
+
+              transform.value = (percentage > 100) ? 100 : percentage
+
+
+              chart.prev = Object.clone(current)
+              chart.prev.value = percentage
+
+
+            }
+            else{
+              transform.timestamp = chart.prev.timestamp
+              transform.value = chart.prev.value
+            }
+
+            transformed.push(transform)
+            // console.log('KEY', transform)
+
+          })
+
+
+          // if(chart.prev.timestamp != current.timestamp){
+          return transformed
+
+
         }
       },
 
