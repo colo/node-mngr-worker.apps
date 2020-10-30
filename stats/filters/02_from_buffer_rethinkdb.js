d6dae6a3d7bc57e633acc56ea498856a6b63fed7
Luciano Bueno <glamero@gmail.com>
2020-10-30 13:24:29 -0300

    bump

--- stats/filters/02_from_buffer_rethinkdb.js
+++ stats/filters/02_from_buffer_rethinkdb.js
@@ -100,98 +100,104 @@ module.exports = function(payload){
         // debug('3rd filter DOC %o', doc)
         // process.exit(1)
 
-        let ts = roundMilliseconds(Date.now())
-        // const ts = buffer.metadata.timestamp + _index
-        let new_doc = Object.clone(template_doc)
+        if(doc.query_engine !== undefined){
+          let ts = roundMilliseconds(Date.now())
+          // const ts = buffer.metadata.timestamp + _index
+          let new_doc = Object.clone(template_doc)
 
-        new_doc.metadata.tag.push(doc.server)
-        new_doc.metadata.tag = new_doc.metadata.tag.combine(doc.id)
-        new_doc.metadata.host = host
-        new_doc.metadata.timestamp = ts
-        new_doc.metadata.id = host +'.'+new_doc.metadata.id + '.' + _index
+          new_doc.metadata.tag.push(doc.server)
+          new_doc.metadata.tag = new_doc.metadata.tag.combine(doc.id)
+          new_doc.metadata.host = host
+          new_doc.metadata.timestamp = ts
+          new_doc.metadata.id = host +'.'+new_doc.metadata.id + '.' + _index
+          
+          let clients_doc = Object.clone(new_doc)
 
-        let clients_doc = Object.clone(new_doc)
+          clients_doc.metadata.tag.push('clients')
+          if(server_id !== undefined)
+            clients_doc.metadata.path += '.'+server_id
 
-        clients_doc.metadata.tag.push('clients')
-        if(server_id !== undefined)
-          clients_doc.metadata.path += '.'+server_id
+          clients_doc.metadata.path += '.clients'
+          clients_doc.metadata.id +='.clients@'+ts
+          clients_doc.data = {
+            connections: doc.query_engine.client_connections,
+            active: doc.query_engine.clients_active
+          }
+          clients_doc.id = clients_doc.metadata.id
+          // debug('3rd filter DOC %o', doc)
+          // process.exit(1)
 
-        clients_doc.metadata.path += '.clients'
-        clients_doc.metadata.id +='.clients@'+ts
-        clients_doc.data = {
-          connections: doc.query_engine.client_connections,
-          active: doc.query_engine.clients_active
+          sanitize_filter(
+            clients_doc,
+            opts,
+            pipeline.output.bind(pipeline),
+            pipeline
+          )
+
+          let queries_doc = Object.clone(new_doc)
+
+          queries_doc.metadata.tag.push('queries')
+          if(server_id !== undefined)
+            queries_doc.metadata.path += '.'+server_id
+
+          queries_doc.metadata.path += '.queries'
+          queries_doc.metadata.id +='.queries@'+ts
+          queries_doc.data = {
+            per_sec: doc.query_engine.queries_per_sec,
+            total: doc.query_engine.queries_total
+          }
+          queries_doc.id = queries_doc.metadata.id
+          sanitize_filter(
+            queries_doc,
+            opts,
+            pipeline.output.bind(pipeline),
+            pipeline
+          )
+
+          let read_doc = Object.clone(new_doc)
+
+          read_doc.metadata.tag.push('read_docs')
+          if(server_id !== undefined)
+            read_doc.metadata.path += '.'+server_id
+
+          read_doc.metadata.path += '.read_docs'
+          read_doc.metadata.id +='.read_docs@'+ts
+          read_doc.data = {
+            per_sec: doc.query_engine.read_docs_per_sec,
+            total: doc.query_engine.read_docs_total
+          }
+          read_doc.id = read_doc.metadata.id
+          sanitize_filter(
+            read_doc,
+            opts,
+            pipeline.output.bind(pipeline),
+            pipeline
+          )
+
+          let write_doc = Object.clone(new_doc)
+
+          write_doc.metadata.tag.push('written_docs')
+          if(server_id !== undefined)
+            write_doc.metadata.path += '.'+server_id
+
+          write_doc.metadata.path += '.written_docs'
+          write_doc.metadata.id +='.written_docs@'+ts
+          write_doc.data = {
+            per_sec: doc.query_engine.written_docs_per_sec,
+            total: doc.query_engine.written_docs_total
+          }
+          write_doc.id = write_doc.metadata.id
+          sanitize_filter(
+            write_doc,
+            opts,
+            pipeline.output.bind(pipeline),
+            pipeline
+          )
         }
-        clients_doc.id = clients_doc.metadata.id
-        // debug('3rd filter DOC %o', doc)
-        // process.exit(1)
 
-        sanitize_filter(
-          clients_doc,
-          opts,
-          pipeline.output.bind(pipeline),
-          pipeline
-        )
 
-        let queries_doc = Object.clone(new_doc)
 
-        queries_doc.metadata.tag.push('queries')
-        if(server_id !== undefined)
-          queries_doc.metadata.path += '.'+server_id
 
-        queries_doc.metadata.path += '.queries'
-        queries_doc.metadata.id +='.queries@'+ts
-        queries_doc.data = {
-          per_sec: doc.query_engine.queries_per_sec,
-          total: doc.query_engine.queries_total
-        }
-        queries_doc.id = queries_doc.metadata.id
-        sanitize_filter(
-          queries_doc,
-          opts,
-          pipeline.output.bind(pipeline),
-          pipeline
-        )
-
-        let read_doc = Object.clone(new_doc)
-
-        read_doc.metadata.tag.push('read_docs')
-        if(server_id !== undefined)
-          read_doc.metadata.path += '.'+server_id
-
-        read_doc.metadata.path += '.read_docs'
-        read_doc.metadata.id +='.read_docs@'+ts
-        read_doc.data = {
-          per_sec: doc.query_engine.read_docs_per_sec,
-          total: doc.query_engine.read_docs_total
-        }
-        read_doc.id = read_doc.metadata.id
-        sanitize_filter(
-          read_doc,
-          opts,
-          pipeline.output.bind(pipeline),
-          pipeline
-        )
-
-        let write_doc = Object.clone(new_doc)
-
-        write_doc.metadata.tag.push('written_docs')
-        if(server_id !== undefined)
-          write_doc.metadata.path += '.'+server_id
-
-        write_doc.metadata.path += '.written_docs'
-        write_doc.metadata.id +='.written_docs@'+ts
-        write_doc.data = {
-          per_sec: doc.query_engine.written_docs_per_sec,
-          total: doc.query_engine.written_docs_total
-        }
-        write_doc.id = write_doc.metadata.id
-        sanitize_filter(
-          write_doc,
-          opts,
-          pipeline.output.bind(pipeline),
-          pipeline
-        )
 
       })
     }
